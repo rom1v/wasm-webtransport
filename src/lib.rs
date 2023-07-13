6385701868e5f6fc47f1703885dec5030df1c6e1
Romain Vimont <rom@rom1v.com>
2023-07-13 11:54:53 +0200

    Call javascript via web_sys

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,23 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
+
 #[wasm_bindgen]
 pub fn hello() {
     console::log_1(&JsValue::from_str("Hello world!"));
