0200f4405802531bb9f961fd5e7e446583fafe39
Romain Vimont <rom@rom1v.com>
2023-07-13 09:54:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+wasm-pack build --target web
+python -m http.server 4242
+```
+
+<https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_wasm>
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn hello() {
+    console::log_1(&JsValue::from_str("Hello world!"));
+}
